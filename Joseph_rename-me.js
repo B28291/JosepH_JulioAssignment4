6788b8bc579f2ea7db6a8a9b0a69f50a9c1d2ad6
Julio <Juliojoseph718@yahoo.com>
2014-02-27 20:52:12 -0500

    first commit

---
+++ Joseph_rename-me.js
@@ -0,0 +1,44 @@
+//alert("JavaScript works!");
+
+//functions 
+
+
+//string 
+
+var phoneNumVal = function(phoneNumber){
+var valcharaters = "1234567890-"
+var	phoneNumlength = phoneNumber.length
+
+	for (i = 0; i < phoneNumlength; i++){
+		
+		var charCheck = phoneNumber.charAt(i);
+		var charReturn = valcharaters.indexof(ch1);
+		
+		};
+	
+	if(charReturn == -1){
+		
+		return false 
+		
+		}else { return true };
+		
+		
+	
+	
+	}; 
+	
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//main code 
+phoneNumPattern(201-212-1844);
